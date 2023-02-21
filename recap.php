1da695a6822d5bc87717c1bdfb9b5eb2927765c2
anthony spiteri <anthonyspiteri@hotmail.fr>
2023-02-21 12:00:00 +0100

    switch case

--- recap.php
+++ recap.php
@@ -4,57 +4,63 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Récapitulatif des produits</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Récapitulatif des produits</title>
 </head>
+
 <body>
 
-    <?php
+	<?php
 
 
-     if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-        echo "<p>Aucun produit en session...</p>";
-     }else{
-        echo "<table>",
-                "<thead>",
-                    "<tr>",
-                        "<th>#</th>",
-                        "<th>Nom</th>",
-                        "<th>Prix</th>",
-                        "<th>Quantité</th>",
-                        "<th>Total</th>",
-                     "</tr>",
-                 "</thead>",
-                 "<tbody>";
-        $totalGeneral = 0;
-        $quantiteTotal = 0;
-        foreach($_SESSION['products'] as $index => $product){
-                echo "<tr>",
-                        "<td>" .$index."</td>",
-                        "<td>" .$product['name']."</td>",
-                        "<td>" .number_format($product['price'],2, ",", "&nbsp;")."&nbsp;€</td>", //number_format( variable à modifier, nombre de décimales souhaité, caractère séparateur décimal, caractère séparateur de milliers5
-                        "<td>" .$product['qtt']."</td>",
-                        "<td>" .number_format($product['total'],2, ",", "&nbsp;")."&nbsp;€</td>",
-                     "</tr>";
-                     $totalGeneral += $product['total'];
-                     $quantiteTotal += $product['qtt'];
-        }
+	if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+		echo "<p>Aucun produit en session...</p>";
+	} else {
+		echo "<table>",
+		"<thead>",
+		"<tr>",
+		"<th>#</th>",
+		"<th>Nom</th>",
+		"<th>Prix</th>",
+		"<th>Quantité</th>",
+		"<th>Total</th>",
+		"</tr>",
+		"</thead>",
+		"<tbody>";
+		// $totalGeneral = 0;
+		$quantiteTotal = 0;
+		foreach ($_SESSION['products'] as $index => $product) {
+			echo "<tr>",
+			"<td>" . $index . "</td>",
+			"<td>" . $product['name'] . "</td>",
+			"<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>", //number_format( variable à modifier, nombre de décimales souhaité, caractère séparateur décimal, caractère séparateur de milliers5
+			"<td>" . $product['qtt'] . "</td>",
+			"<td>" . number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
+			"</tr>";
+			// $totalGeneral += $product['total'];
+			$quantiteTotal += $product['qtt'];
+		}
 
-        echo "<tr>",
-                "<td colspan=4>Total général: </td>",
-                "<td><strong>".number_format($totalGeneral,2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-              "</tr>",
-            "</tbody",
-            "</table>";
+		var_dump($_SESSION['products']);
 
-            echo "<p style='text-align: center; margin-top: 1rem;'>Nombre de produits dans le panier: <strong>" .$quantiteTotal."</strong></p";
-                        
-                        
-     }
-      ?>
+
+		echo "<tr>",
+		"<td colspan=4>Total général: </td>",
+		"<td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</strong></td>",
+		"</tr>",
+		"</tbody",
+		"</table>";
+
+		echo "<button action='deleteAll'>Vider le panier</button>";
+
+		echo "<p style='text-align: center; margin-top: 1rem;'>Nombre de produits dans le panier: <strong>" . $quantiteTotal . "</strong></p";
+	}
+	?>
 </body>
+
 </html>
--- traitement.php
+++ traitement.php
@@ -1,23 +1,42 @@
 <?php
-session_start(); 
+// SESSION : Les sessions sont un moyen simple de stocker des données individuelles pour chaque utilisateur en utilisant un identifiant de session unique. Elles peuvent être utilisées pour récuperer entre plusieurs pages. une SESSION est stocké coté serveur.
+//COOKIE: Les cookies sont un mécanisme d'enregistrement d'informations sur le client, et de lecture de ces informations. Ce système permet d'identifier et de suivre les visiteurs. Vous pouvez envoyer un cookie avec la fonction setcookie() ou setrawcookie(). on stock coté navigateur.
+//SUPERGLOBALE: Plusieurs variables prédéfinies en PHP sont "superglobales", ce qui signifie qu'elles sont disponibles quel que soit le contexte du script. Il est inutile de faire global $variable; avant d'y accéder dans les fonctions ou les méthodes. Les variables superglobales sont : $GLOBALS. (exemple: $_SESSION)
+//REQUETE HTTP: Une requête HTTP, acronyme de « Hypertext Transfer Protocol » renvoie au protocole HTTP utilisé par le navigateur web pour consulter un site internet. L'ordinateur de l'internaute, via un navigateur, envoie une requête au serveur web, qui lui apporte alors immédiatement une réponse en affichant la page web demandée.
 
-if(isset($_POST['submit'])){ // verifier s'il y a une cle submit
- $name= filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING); // sécurité filtre les champs
- $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
- $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
- if($name && $price && $qtt){ // verifier si les filtres ont fonctionné
-    $product = [
-        "name" => $name,
-        "price" => $price,
-        "qtt" => $qtt,
-        "total" => $price * $qtt
-    ];
+session_start();
 
-    // enregistré le tableau product
-    $_SESSION['products'][] = $product;
- }
+if (isset($_GET['action'])) { // si action est déclaré et différente de NULL // si le mot action se trouve dans l'url
+
+	switch ($_GET['action']) {
+		case "add": // appliquer la fonction add 
+			if (isset($_POST['submit'])) { // verifier s'il y a une cle submit
+				// les filtres permettent de ce prémunir d'une faille XSS (injecter du code HTML et/ou Javascript) dans une variable.
+				$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS); // sécurité filtre les champs
+				$price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+				$qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+
+				if ($name && $price && $qtt) { // verifier si les filtres ont fonctionné
+					$product = [
+						"name" => $name,
+						"price" => $price,
+						"qtt" => $qtt,
+						"total" => $price * $qtt
+					];
+
+					// enregistré le tableau product
+					$_SESSION['products'][] = $product;
+				}
+			}
+			break;
+		case "deleteAll":
+			unset($_SESSION['products']);
+			break;
+	}
 }
 
+
+
 header("location:index.php"); // redirection
-?>
